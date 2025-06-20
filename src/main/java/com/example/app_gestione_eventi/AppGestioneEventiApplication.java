2656f2ce0a1aa276b4f9a3fbf92358355dfb0987
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-06-20 09:59:06 +0200

    Initial commit

---
+++ src/main/java/com/example/app_gestione_eventi/AppGestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_gestione_eventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGestioneEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/app_gestione_eventi/AppGestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app_gestione_eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
